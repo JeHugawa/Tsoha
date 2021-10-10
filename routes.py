56d4482d3c5c7351a9653fe33b3400900541fab9
Jasper Hjorth <jasper.hjorth@gmail.com>
2021-10-10 23:24:07 +0300

    Add creating new threads

--- routes.py
+++ routes.py
@@ -57,7 +57,7 @@ def logout():
     return redirect("/")
 
 
-@app.route("/<int:board_id>", methods=["GET"])
+@app.route("/<int:board_id>", methods=["GET","POST"])
 def board(board_id):
     if request.method == "GET":
         sql = "SELECT name, description FROM boards WHERE id=:board_id"
@@ -65,10 +65,20 @@ def board(board_id):
         info = result.fetchone()
         sql = "SELECT topic, board_id, id, sent FROM threads where board_id=:board_id"
         result = db.session.execute(sql, {"board_id":board_id})
-        return render_template("boards.html",board=result, metainfo=info)
-
-
-
+        return render_template("boards.html",board=result, metainfo=info, board_id=board_id)
+    if request.method == "POST":
+        topic = request.form["topic"]
+        message = request.form["message"]    
+        sql = "INSERT INTO threads (board_id, topic,sent) VALUES (:board_id, :topic, CURRENT_TIMESTAMP)"
+        db.session.execute(sql, {"board_id":board_id, "topic":topic})
+        db.session.commit()
+        result = db.session.execute("SELECT ID FROM threads ORDER BY id DESC LIMIT 1")
+        thread_id = result.fetchone()
+        thread_id = thread_id[0]
+        sql = "INSERT INTO message (thread_id, poster_id, sent, message) VALUES (:thread_id, :poster_id, CURRENT_TIMESTAMP, :message)"
+        db.session.execute(sql, {"thread_id":thread_id, "poster_id":session["user_id"], "message":message})
+        db.session.commit()
+        return redirect(f"/{board_id}")
 
 @app.route("/<int:board_id>/<int:thread_id>", methods=["GET","POST"])
 def page(board_id, thread_id):
